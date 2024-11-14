e11ca8c203646f1b658e040c4dc0aa245b8ce602
d-anasx <djamaouianas@gmail.com>
2024-11-14 23:12:30 +0100

    second commit

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import avatar from './assets/avatar.png'; 
 import { Camera } from 'lucide-react';
@@ -9,12 +7,10 @@ import { useEffect } from 'react';
 function UserProfile() {
   const [JsonData, setJsonData] = useState(externalJson);
   useEffect(() => {
-        setJsonData((prevData) => ({
-          ...prevData,
-          photo: avatar,
-        }));
-     
-  
+    setJsonData((prevData) => ({
+      ...prevData,
+      photo: avatar,
+    }));
   }, []);
 
   const [isEditing, setIsEditing] = useState(false); 
@@ -36,14 +32,14 @@ function UserProfile() {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 mx-auto">
-      <div className="max-w-4xl p-8 bg-gray-800 rounded-lg shadow-2xl">
-        <div className="flex items-center gap-6 mb-8">
+    <div className="h-screen flex items-center justify-center bg-gray-900 mx-auto p-4 sm:p-8">
+      <div className="w-full max-w-2xl sm:max-w-4xl p-4 sm:p-8 bg-gray-800 rounded-lg shadow-2xl">
+        <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6 mb-4 sm:mb-8">
           <div className="relative">
             <img
               src={JsonData.photo}
               alt="JsonData Profile"
-              className="w-32 h-32 rounded-full border-4 border-gray-600 object-cover transition duration-300 ease-in-out transform hover:scale-105"
+              className="w-20 h-20 sm:w-32 sm:h-32 rounded-full border-4 border-gray-600 object-cover transition duration-300 ease-in-out transform hover:scale-105"
             />
             <label className="absolute bottom-0 right-0 p-2 bg-blue-500 rounded-full cursor-pointer hover:bg-blue-600 transition duration-300">
               <Camera className="text-2xl text-white" />
@@ -52,7 +48,7 @@ function UserProfile() {
                 accept="image/*"
                 className="hidden"
                 onChange={handlePhotoUpload}
-                disabled={!isEditing} // Only allow photo upload when in edit mode
+                disabled={!isEditing}
               />
             </label>
           </div>
@@ -84,96 +80,88 @@ function UserProfile() {
           </div>
         </div>
 
-        <table className="w-full">
-          <tbody>
-            <tr>
-              <td className="px-6 py-4 font-semibold">Email</td>
-              <td className="px-6 py-4">
-                {isEditing ? (
-                  <input
-                    type="email"
-                    name="email"
-                    value={JsonData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    className="input input-ghost w-full max-w-xs"
-                  />
-                ) : (
-                  <p className="text-lg">{JsonData.email}</p>
-                )}
-              </td>
-              <td className="px-6 py-4 font-semibold">Phone</td>
-              <td className="px-6 py-4">
-                {isEditing ? (
-                  <input
-                    type="text"
-                    name="phone"
-                    value={JsonData.phone}
-                    onChange={handleChange}
-                    placeholder="Phone"
-                    className="input input-ghost w-full max-w-xs"
-                  />
-                ) : (
-                  <p className="text-lg">{JsonData.phone}</p>
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td className="px-6 py-4 font-semibold">Address</td>
-              <td className="px-6 py-4">
-                {isEditing ? (
-                  <input
-                    type="text"
-                    name="address"
-                    value={JsonData.address}
-                    onChange={handleChange}
-                    placeholder="Address"
-                    className="input input-ghost w-full max-w-xs"
-                  />
-                ) : (
-                  <p className="text-lg">{JsonData.address}</p>
-                )}
-              </td>
-              <td className="px-6 py-4 font-semibold">Website</td>
-              <td className="px-6 py-4">
-                {isEditing ? (
-                  <input
-                    type="url"
-                    name="website"
-                    value={JsonData.website}
-                    onChange={handleChange}
-                    placeholder="Website"
-                    className="input input-ghost w-full max-w-xs"
-                  />
-                ) : (
-                  <p className="text-lg">{JsonData.website}</p>
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td className="px-6 py-4 font-semibold">Bio</td>
-              <td className="px-6 py-4" colSpan="3">
-                {isEditing ? (
-                  <textarea
-                    rows="4"
-                    name="bio"
-                    value={JsonData.bio}
-                    onChange={handleChange}
-                    placeholder="Bio"
-                    className="textarea textarea-ghost w-full"
-                  />
-                ) : (
-                  <p className="text-lg">{JsonData.bio}</p>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <span className="font-semibold">Email</span>
+            {isEditing ? (
+              <input
+                type="email"
+                name="email"
+                value={JsonData.email}
+                onChange={handleChange}
+                placeholder="Email"
+                className="input input-ghost w-full max-w-xs"
+              />
+            ) : (
+              <p className="text-lg">{JsonData.email}</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <span className="font-semibold">Phone</span>
+            {isEditing ? (
+              <input
+                type="text"
+                name="phone"
+                value={JsonData.phone}
+                onChange={handleChange}
+                placeholder="Phone"
+                className="input input-ghost w-full max-w-xs"
+              />
+            ) : (
+              <p className="text-lg">{JsonData.phone}</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <span className="font-semibold">Address</span>
+            {isEditing ? (
+              <input
+                type="text"
+                name="address"
+                value={JsonData.address}
+                onChange={handleChange}
+                placeholder="Address"
+                className="input input-ghost w-full max-w-xs"
+              />
+            ) : (
+              <p className="text-lg">{JsonData.address}</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <span className="font-semibold">Website</span>
+            {isEditing ? (
+              <input
+                type="url"
+                name="website"
+                value={JsonData.website}
+                onChange={handleChange}
+                placeholder="Website"
+                className="input input-ghost w-full max-w-xs"
+              />
+            ) : (
+              <p className="text-lg">{JsonData.website}</p>
+            )}
+          </div>
+          <div className="flex flex-col sm:col-span-2">
+            <span className="font-semibold">Bio</span>
+            {isEditing ? (
+              <textarea
+                rows="4"
+                name="bio"
+                value={JsonData.bio}
+                onChange={handleChange}
+                placeholder="Bio"
+                className="textarea textarea-ghost w-full"
+              />
+            ) : (
+              <p className="text-lg">{JsonData.bio}</p>
+            )}
+          </div>
+        </div>
 
         <div className="flex justify-between mt-8">
           <button
             onClick={() => setIsEditing(!isEditing)}
-            className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-full sm:w-auto px-4 sm:px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105"
           >
             {isEditing ? 'Save Changes' : 'Edit Profile'}
           </button>
@@ -184,4 +172,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
